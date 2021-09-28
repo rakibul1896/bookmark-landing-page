2fbf750cce003fe74fa80939e4726b07da1ca945
rakibul1896 <rhkhan9618@gmail.com>
2021-09-28 16:59:15 +0600

    margin changes

--- src/components/downloadExtensions/DownloadExtension.jsx
+++ src/components/downloadExtensions/DownloadExtension.jsx
@@ -45,7 +45,7 @@ const DownloadExtension = () => {
               sm={6}
               md={4}
               lg={3}
-              className={`shadow rounded-4 p-0 pb-4 mx-3 mt-5 mt-md-${val.top}`}
+              className={`shadow rounded-4 p-0 pb-4 mx-4 mt-5 mt-xl-${val.top}`}
               key={ind}
             >
               <img src={val.img} alt="" className="py-5" />
